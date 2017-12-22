7ed990cf723bbc238e94edfa5c0d8a3cb1ec463f
joxar <mfpirlo@yahoo.co.jp>
2017-12-23 04:10:54 +0900

    a

--- brain.sh
+++ brain.sh
@@ -3,7 +3,7 @@ set -e -x
 echo "=== brain.sh ==="
 
 cd ..
-readonly FACE_OUTPUT_DIR="${FACE_HOME}/output"
+readonly FACE_OUTPUT_DIR="src-face/output"
 readonly FACE_FILE="*.jpg"
 readonly VOICE_DIR=${VOICE_HOME}
 
@@ -13,7 +13,7 @@ arr_len="${#array[@]}"
 # face
 export PYTHONPATH="/usr/local/lib/python2.7/site-packages/:$PYTHONPATH"
 export PYTHONPATH="/usr/lib64/python2.7/site-packages:$PYTHONPATH"
-cd ${FACE_DIR}
+cd "src-face"
 /usr/bin/python opencv-face-camera.py &
 
 # watch pict existence
